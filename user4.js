fff8e76dd080b4222737280af242d6bfc3f51e48
aaravthecoder123 <162795685+aaravthecoder123@users.noreply.github.com>
2024-10-17 21:06:47 +0530

    Add files via upload

---
+++ user4.js
@@ -0,0 +1,12 @@
+const accountId = 1
+let accountemail = "aaravsri09@gmail.com"
+var accountPassword = "aarav5fff"
+accountCity = "Delhi"
+
+ accountemail = "aaravsri09@gmail.com"
+ accountPassword = "aarav5fff"
+ accountCity = "Delhi"
+// accountId = 2
+ 
+ console.table([accountId,accountemail,accountPassword,accountCity])
+
